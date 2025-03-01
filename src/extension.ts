c376ada88623bc7192069c9c4ac05492cb7ee6ef
dhruv0127 <dhruv10parekh29@gmail.com>
2025-03-02 01:59:49 +0530

    Section Creation code added

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,110 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "quickstruct-liquid-sections" is now active!');
+	const createFileCommand = vscode.commands.registerCommand('quickstruct-liquid-sections.createSectionFile', async () => {
+		const fileNameInput = await vscode.window.showInputBox({
+			prompt: 'Enter the name of the new section file',
+			placeHolder: 'example'
+		});
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('quickstruct-liquid-sections.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from QuickStruct Liquid Sections!');
+		if (fileNameInput) {
+			const fileName = fileNameInput.replace(/[\s_&$%@#]/g, '-').toLowerCase();
+
+			const workspaceFolders = vscode.workspace.workspaceFolders;
+			if (workspaceFolders) {
+				const sectionsFolderPath = path.join(workspaceFolders[0].uri.fsPath, 'sections');
+				const assetsFolderPath = path.join(workspaceFolders[0].uri.fsPath, 'assets');
+
+				if (!fs.existsSync(sectionsFolderPath)) {
+					fs.mkdirSync(sectionsFolderPath);
+				}
+				if (!fs.existsSync(assetsFolderPath)) {
+					fs.mkdirSync(assetsFolderPath);
+				}
+
+				const sectionFilePath = path.join(sectionsFolderPath, `${fileName}.liquid`);
+				let sectionContent = ``;
+
+				let stylesheetPath = '';
+				let scriptPath = '';
+
+				const addStylesheet = await vscode.window.showQuickPick(['Yes', 'No'], { placeHolder: 'Do you want to add a stylesheet file to the assets folder?' });
+				if (addStylesheet === 'Yes') {
+					stylesheetPath = path.join(assetsFolderPath, `${fileName}-stylesheet.css`);
+					fs.writeFileSync(stylesheetPath, '');
+					sectionContent += `{{ '${fileName}-stylesheet.css' | asset_url | stylesheet_tag }}`;
+				}
+				
+				sectionContent += `\n`;
+				sectionContent += `\n`;
+
+				const scriptOption = await vscode.window.showQuickPick(['Async', 'Defer', 'Skip JavaScript'], { placeHolder: 'Define JavaScript load behavior (or skip adding a script)' });
+				if (scriptOption !== 'Skip JavaScript') {
+					scriptPath = path.join(assetsFolderPath, `${fileName}-javascript.js`);
+					fs.writeFileSync(scriptPath, '');
+					const scriptTag = scriptOption === 'Async' ? `async: true` : `defer: true`;
+					sectionContent += `\n{{ '${fileName}-javascript.js' | asset_url | script_tag: ${scriptTag} }}\n`;
+				}
+
+				sectionContent += `
+{% schema %}
+{
+	"name": "${fileName}",
+	"class": "${fileName}",
+	"settings": [],
+	"presets": [
+		{
+			"name": "${fileName}"
+		}
+	]
+}
+{% endschema %}
+`;
+
+				fs.writeFileSync(sectionFilePath, sectionContent);
+
+				if (stylesheetPath) {
+					const stylesheetUri = vscode.Uri.file(stylesheetPath);
+					vscode.workspace.openTextDocument(stylesheetUri).then(doc => vscode.window.showTextDocument(doc, { preview: false }));
+					await vscode.window.withProgress({
+						location: vscode.ProgressLocation.Notification,
+						title: `CSS ${fileName} File created and linked in liquid`
+					}, async (progress) => {
+						await new Promise(resolve => setTimeout(resolve, 200));
+					});
+				}
+
+				if (scriptPath) {
+					const scriptUri = vscode.Uri.file(scriptPath);
+					vscode.workspace.openTextDocument(scriptUri).then(doc => vscode.window.showTextDocument(doc, { preview: false }));
+					await vscode.window.withProgress({
+						location: vscode.ProgressLocation.Notification,
+						title: `Js ${fileName} File created and linked in liquid`
+					}, async (progress) => {
+						await new Promise(resolve => setTimeout(resolve, 200));
+					});
+				}
+
+				
+				// Open all created files in persistent tabs
+				const sectionUri = vscode.Uri.file(sectionFilePath);
+				vscode.workspace.openTextDocument(sectionUri).then(doc => vscode.window.showTextDocument(doc, { preview: false }));
+
+				await vscode.window.withProgress({
+					location: vscode.ProgressLocation.Notification,
+					title: `Liquid File ${fileName} Created`
+				}, async (progress) => {
+					await new Promise(resolve => setTimeout(resolve, 2000));
+				});
+				
+			}
+		}
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(createFileCommand);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
